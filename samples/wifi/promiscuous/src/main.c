4aaf0886b3c230b73c40846fdaf51d45e4a2e6f8
Chiranjeevi Srikakulapu <chiranjeevi.srikakulapu@nordicsemi.no>
2024-03-15 12:09:43 +0530

    samples: wifi: promiscuous: Added Wi-Fi connection
    Updated wifi connection logic on top of promiscuous mode
    
    Signed-off-by: Chiranjeevi Srikakulapu <chiranjeevi.srikakulapu@nordicsemi.no>

--- samples/wifi/promiscuous/src/main.c
+++ samples/wifi/promiscuous/src/main.c
@@ -13,9 +13,7 @@
 #include <zephyr/net/wifi_mgmt.h>
 LOG_MODULE_REGISTER(promiscuous, CONFIG_LOG_DEFAULT_LEVEL);
 
-#ifdef CONFIG_NET_CONFIG_SETTINGS
-#include <zephyr/net/net_config.h>
-#endif
+#include "wifi_connection.h"
 
 #define ENABLE_PROMISCUOUS_MODE 1
 #define DISABLE_PROMISCUOUS_MODE 0
@@ -50,42 +48,6 @@ static int wifi_set_reg(void)
 	return 0;
 }
 
-static int wifi_set_channel(void)
-{
-	struct net_if *iface;
-	struct wifi_channel_info channel_info = { 0 };
-	int ret;
-
-	channel_info.oper = WIFI_MGMT_SET;
-
-	iface = net_if_get_first_wifi();
-	if (iface == NULL) {
-		LOG_ERR("No Wi-Fi interface found");
-		return -1;
-	}
-
-	channel_info.if_index = net_if_get_by_iface(iface);
-	channel_info.channel = CONFIG_PROMISCUOUS_MODE_CHANNEL;
-	if ((channel_info.channel < WIFI_CHANNEL_MIN) ||
-	    (channel_info.channel > WIFI_CHANNEL_MAX)) {
-		LOG_ERR("Invalid channel number %d. Range is %d-%d",
-			channel_info.channel, WIFI_CHANNEL_MIN, WIFI_CHANNEL_MAX);
-		return -1;
-	}
-
-	ret = net_mgmt(NET_REQUEST_WIFI_CHANNEL, iface,
-		       &channel_info, sizeof(channel_info));
-	if (ret) {
-		LOG_ERR(" Channel setting failed %d Channel %d\n", ret, channel_info.channel);
-		return -1;
-	}
-
-	LOG_INF("Wi-Fi channel set to %d for interface %d",
-		channel_info.channel, channel_info.if_index);
-
-	return 0;
-}
-
 static int wifi_set_mode(void)
 {
 	int ret;
@@ -113,22 +75,6 @@ int main(void)
 {
 	int ret;
 
-#ifdef CONFIG_NET_CONFIG_SETTINGS
-	/* Without this, DHCPv4 starts on first interface and if that is not Wi-Fi or
-	 * only supports IPv6, then its an issue. (E.g., OpenThread)
-	 *
-	 * So, we start DHCPv4 on Wi-Fi interface always, independent of the ordering.
-	 */
-	/* TODO: Replace device name with DTS settings later */
-	const struct device *dev = device_get_binding("wlan0");
-	struct net_if *wifi_iface = net_if_lookup_by_dev(dev);
-
-	/* As both are Ethernet, we need to set specific interface*/
-	net_if_set_default(wifi_iface);
-
-	net_config_init_app(dev, "Initializing network");
-#endif
-
 	ret = wifi_set_mode();
 	if (ret) {
 		return -1;
@@ -139,9 +85,9 @@ int main(void)
 		return -1;
 	}
 
-	ret = wifi_set_channel();
-	if (ret) {
-		return -1;
+	ret = try_wifi_connect();
+	if (ret < 0) {
+		return ret;
 	}
 
 	return 0;
